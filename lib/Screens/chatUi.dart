781c966f5ad34e4e52715f48e9f25c9c7a9d7e7a
Sagar Dhadke <peaceunique2003@gmail.com>
2025-03-27 23:50:25 +0530

    Added Basic Structure and assets

---
+++ lib/Screens/chatUi.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyChatUI extends StatelessWidget {
+  const MyChatUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
