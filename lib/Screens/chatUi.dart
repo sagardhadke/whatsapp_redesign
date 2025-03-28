983c189926804f6c49685de6a26c65f29493621b
Sagar Dhadke <peaceunique2003@gmail.com>
2025-03-28 21:31:27 +0530

    Done AppBar

--- lib/Screens/chatUi.dart
+++ lib/Screens/chatUi.dart
@@ -6,7 +6,32 @@ class MyChatUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Color(0XFF0B1014),
+      appBar: AppBar(
+        backgroundColor: Color(0XFF0B1014),
+        foregroundColor: Colors.white,
+        title: Text(
+          "WhatsApp",
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        actions: [
+          Icon(Icons.qr_code_2_outlined),
+          SizedBox(
+            width: 5,
+          ),
+          Icon(Icons.camera_alt_outlined),
+          SizedBox(
+            width: 5,
+          ),
+          Icon(Icons.more_vert),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
     );
   }
 }
